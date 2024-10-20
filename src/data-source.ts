f148fa6e07b8c4d2a66dc5d97420d4fe31ad3094
Castas115 <joncastas@gmail.com>
2024-10-20 11:33:11 +0200

    .env applied

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,13 +2,16 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
+
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
+    host: DB_HOST,
+    port: parseInt(DB_PORT || "3306"),
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+  
     synchronize: true,
     logging: false,
     entities: [User],
