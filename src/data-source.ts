eb5c94ec3cd3e987f955d86577d969f0a44b6dd9
Castas115 <joncastas@gmail.com>
2024-10-20 11:45:22 +0200

    fixes to .env

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
-
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: DB_HOST,
