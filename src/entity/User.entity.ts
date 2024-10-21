2a06204d89e65c8e521e950b444ebef4c6939b8b
Castas115 <joncastas@gmail.com>
2024-10-21 12:41:56 +0200

    regular ID

--- src/entity/User.entity.ts
+++ src/entity/User.entity.ts
@@ -3,7 +3,7 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 @Entity()
 export class User {
 
-    @PrimaryGeneratedColumn("uuid")
+    @PrimaryGeneratedColumn()
     id: number
 
     @Column({ nullable: false })
