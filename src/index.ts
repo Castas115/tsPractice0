692bc98f8d50beae29f9492ee633a5d7f75a9df5
Castas115 <joncastas@gmail.com>
2024-10-21 12:18:55 +0200

    improved index

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
-import { AppDataSource } from "./data-source"
+import { AppDataSource } from "./data-source";
 import * as express from "express";
-import { Request, Response } from "express";
 import * as dotenv from "dotenv";
-import { userRouter } from "./routes/user.router";
+import { Request, Response } from "express";
+import { userRouter } from "./routes/user.routes";
+import { errorHandler } from "./middleware/error.middleware";
+import "reflect-metadata";
+dotenv.config();
 
 const app = express();
-
-
+app.use(express.json());
+app.use(errorHandler as any);
+const { PORT = 3000 } = process.env;
 app.use("/auth", userRouter);
 
-AppDataSource.initialize().then(async () => {
+app.get("*", (req: Request, res: Response) => {
+  res.status(505).json({ message: "Bad Request" });
+});
 
-
-}).catch(error => console.log(error))
+AppDataSource.initialize()
+  .then(async () => {
+    app.listen(PORT, () => {
+      console.log("Server is running on http://localhost:" + PORT);
+    });
+    console.log("Data Source has been initialized!");
+  })
+  .catch((error) => console.log(error));
