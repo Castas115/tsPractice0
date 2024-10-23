6d3aa0beccc44c4cb0a981e5f899708ea7ddb7f7
Castas115 <joncastas@gmail.com>
2024-10-23 11:23:23 +0200

    removed commented code

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -44,18 +44,4 @@ export class AuthController {
     });
     return res.status(200).json({ ...user, password: undefined });
   }
-
-// export const authorization = (roles: string[]) => {
-//   return async (req: Request, res: Response, next: NextFunction) => {
-//     const userRepo = AppDataSource.getRepository(User);
-//     const user = await userRepo.findOne({
-//       where: { id: req[" currentUser"].id },
-//     });
-//     console.log(user);
-//     if (!roles.includes(user.role)) {
-//       return res.status(403).json({ message: "Forbidden" });
-//     }
-//     next();
-//   };
-// };
 }
